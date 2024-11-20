e24ea95116b88f74e3476fae8ac4023d3986dad0
Aleksandr Misonizhnik <misonijnik@gmail.com>
2024-11-20 17:22:27 +0300

    chore(main):

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
-#include <clang/CIR/Dialect/IR/CIRDialect.h>
-#include <llvm/Support/Casting.h>
-#include <mlir/Dialect/DLTI/DLTIDialect.h.inc>
-#include <mlir/Dialect/LLVMIR/LLVMDialect.h>
+#include "CXX/CIRModule.h"
+#include "CXX/CIRReader.h"
 
+#include "proto/model.pb.h"
+
+#include <clang/CIR/Dialect/IR/CIRDialect.h>
 #include <clang/CIR/Passes.h>
 
+#include <mlir/Dialect/DLTI/DLTIDialect.h.inc>
+#include <mlir/Dialect/LLVMIR/LLVMDialect.h>
 #include <mlir/IR/BuiltinOps.h>
 #include <mlir/IR/Dialect.h>
 #include <mlir/IR/MLIRContext.h>
@@ -13,50 +16,34 @@
 #include <mlir/IR/Visitors.h>
 #include <mlir/Parser/Parser.h>
 
+#include <llvm/ADT/TypeSwitch.h>
+#include <llvm/Support/Casting.h>
+
 #include <iostream>
 
-#include "CXX/CIRModule.h"
-#include "CXX/CIRReader.h"
-
 int main(int argc, char *argv[]) {
-  // mlir::DialectRegistry registry;
-  // registry.insert<cir::CIRDialect, mlir::DLTIDialect,
-  //                 mlir::LLVM::LLVMDialect>();
-
-  // mlir::MLIRContext ctx(registry, mlir::MLIRContext::Threading::DISABLED);
-
-  // mlir::ParserConfig parseConfig(&ctx, /*verifyAfterParse=*/true);
-
-  // auto module = parseSourceFile<mlir::Operation *>(argv[1], parseConfig);
-
-  // std::cout << module->getNumRegions() << std::endl;
-
-  // auto &region = module->getRegion(0);
-
-  // int cnt = 0;
-  // for (auto &block : region) {
-  //   auto &funcList = block.getOperations();
-  //   for (auto &func : funcList) {
-  //     auto &funcRegion = func.getRegion(0);
-  //     auto &funcBody = funcRegion.getBlocks().front().getOperations();
-  //     for (auto &op : funcBody) {
-  //       // if (dynamic_cast<cir::AllocaOp *>(op)) {
-  //       // }
-  //       if (llvm::isa<cir::AllocaOp>(op)) {
-  //         llvm::errs() << "ALLOCA!\n";
-  //       }
-
-  //       // op.getOperand(0)
-  //       ++cnt;
-  //     }
-  //   }
-  // }
   CIRReader reader;
   auto module = reader.loadFromFile(argv[1]);
+  protocir::CIRModule pModule;
+  protocir::CIRModuleID pModuleID;
+  std::string moduleId = "myModule";
+  pModuleID.set_allocated_id(&moduleId);
+  pModule.set_allocated_id(&pModuleID);
+  unsigned long func_idx = 0;
   for (auto func : module.functionsList()) {
-    func.instructionsList();
+    protocir::CIRFunction pFunction;
+    protocir::CIRFunctionID pFunctionID;
+    pFunctionID.set_id(++func_idx);
+    pFunctionID.set_allocated_module_id(&pModuleID);
+    pFunction.set_allocated_id(&pFunctionID);
+    unsigned long inst_line = 0;
+    for (auto inst : func.instructionsList()) {
+      auto pInst = pFunction.add_operations();
+      protocir::CIRAllocaOp pAllocaOp;
+      protocir::CIROpID pOpID;
+      pOpID.set_line(inst_line++);
+      pAllocaOp.set_allocated_base(&pOpID);
+      pInst->set_allocated_alloca(&pAllocaOp);
+    }
   }
-
-  // std::cout << cnt << std::endl;
-  // module->dump();
 }
