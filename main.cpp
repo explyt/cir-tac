40b3ec1454595a2ae5247a4e9289a35f56b142b2
Aleksandr Misonizhnik <misonijnik@gmail.com>
2024-11-22 14:23:57 +0300

    chore(proto): Add `LoadOp` serialization

--- main.cpp
+++ main.cpp
@@ -20,42 +20,80 @@
 #include <llvm/ADT/DenseMap.h>
 #include <llvm/ADT/TypeSwitch.h>
 #include <llvm/Support/Casting.h>
+#include <llvm/Support/ErrorHandling.h>
 
 #include <cinttypes>
 #include <iostream>
 #include <vector>
 
-using indexed_types = llvm::DenseMap<uint64_t, mlir::Type>;
-using intern_type_cache = llvm::DenseMap<mlir::Type, uint64_t>;
+using IDToType = llvm::DenseMap<uint64_t, mlir::Type>;
+using TypeCache = llvm::DenseMap<mlir::Type, uint64_t>;
 
-uint64_t internType(intern_type_cache &cache, mlir::Type type) {
+using IDToOperation = llvm::DenseMap<uint64_t, mlir::Operation *>;
+using OperationCache = llvm::DenseMap<mlir::Operation *, uint64_t>;
+
+uint64_t internType(TypeCache &cache, mlir::Type type) {
   if (!cache.contains(type)) {
     cache[type] = cache.size();
   }
   return cache.at(type);
 }
 
+uint64_t internOperation(OperationCache &cache, mlir::Operation *operation) {
+  if (!cache.contains(operation)) {
+    cache[operation] = cache.size();
+  }
+  return cache.at(operation);
+}
+
 void serializeOperation(mlir::Operation &inst, protocir::CIROp *pInst,
-                        unsigned long &inst_line,
-                        protocir::CIRModuleID pModuleID,
-                        intern_type_cache &internCache) {
+                        protocir::CIRModuleID pModuleID, TypeCache &typeCache,
+                        OperationCache &opCache) {
+  auto instLine = internOperation(opCache, &inst);
   llvm::TypeSwitch<mlir::Operation *>(&inst)
       .Case<cir::AllocaOp>(
-          [&inst_line, pInst, pModuleID, &internCache](cir::AllocaOp op) {
+          [instLine, pInst, pModuleID, &typeCache](cir::AllocaOp op) {
             protocir::CIRAllocaOp pAllocaOp;
-            protocir::CIROpID pOpID;
-            pOpID.set_line(inst_line++);
-            pAllocaOp.mutable_base()->CopyFrom(pOpID);
+            pAllocaOp.mutable_base()->set_line(instLine);
             protocir::CIRTypeID pTypeID;
             pTypeID.mutable_module_id()->CopyFrom(pModuleID);
-            pTypeID.set_id(internType(internCache, op.getAllocaType()));
-            pAllocaOp.mutable_type()->CopyFrom(pTypeID);
+            pTypeID.set_id(internType(typeCache, op.getAllocaType()));
+            pAllocaOp.mutable_alloca_type()->CopyFrom(pTypeID);
             if (op.getAlignment().has_value()) {
               pAllocaOp.set_alignment(op.getAlignment().value());
             }
             pInst->mutable_alloca()->CopyFrom(pAllocaOp);
           })
-      .Default([](mlir::Operation *) {});
+      .Case<cir::BinOp>(
+          [instLine, pInst, pModuleID, &typeCache](cir::BinOp op) {
+            protocir::CIRBinOp pBinOp;
+            pBinOp.mutable_base()->set_line(instLine);
+          })
+      .Case<cir::LoadOp>(
+          [instLine, pInst, pModuleID, &typeCache, &opCache](cir::LoadOp op) {
+            protocir::CIRLoadOp pLoadOp;
+            pLoadOp.mutable_base()->set_line(instLine);
+            auto addressLine =
+                internOperation(opCache, op.getAddr().getDefiningOp());
+            pLoadOp.mutable_address()->set_line(addressLine);
+            auto resultTypeID = internType(typeCache, op.getResult().getType());
+            pLoadOp.mutable_result_type()->set_id(resultTypeID);
+          })
+      .Case<cir::StoreOp>(
+          [instLine, pInst, pModuleID, &typeCache, &opCache](cir::StoreOp op) {
+            protocir::CIRStoreOp pStoreOp;
+          })
+      .Case<cir::ConstantOp>([instLine, pInst, pModuleID, &typeCache,
+                              &opCache](cir::ConstantOp op) {
+        protocir::CIRConstantOp pConstantOp;
+      })
+      .Case<cir::CallOp>([instLine, pInst, pModuleID, &typeCache, &opCache](
+                             cir::CallOp op) { protocir::CIRCallOp pCallOp; })
+      .Case<cir::ReturnOp>(
+          [instLine, pInst, pModuleID, &typeCache, &opCache](cir::ReturnOp op) {
+            protocir::CIRReturnOp pReturnOp;
+          })
+      .Default([](mlir::Operation *) { llvm_unreachable("NIY"); });
 }
 
 int main(int argc, char *argv[]) {
@@ -66,24 +104,24 @@ int main(int argc, char *argv[]) {
   context.appendDialectRegistry(registry);
   context.allowUnregisteredDialects();
 
-  std::filesystem::path apath = argv[1];
+  std::filesystem::path relPath = argv[1];
 
-  auto absPath = std::filesystem::absolute(apath);
+  auto absPath = std::filesystem::absolute(relPath);
   if (!std::filesystem::exists(absPath)) {
     throw std::runtime_error("missing path given");
   }
 
   mlir::ParserConfig parseConfig(&context);
   auto module =
-      mlir::parseSourceFile<mlir::ModuleOp>(apath.c_str(), parseConfig);
+      mlir::parseSourceFile<mlir::ModuleOp>(relPath.c_str(), parseConfig);
   protocir::CIRModule pModule;
   protocir::CIRModuleID pModuleID;
   std::string moduleId = "myModule";
   *pModuleID.mutable_id() = moduleId;
   pModule.mutable_id()->CopyFrom(pModuleID);
   unsigned long func_idx = 0;
-  indexed_types indexToType;
-  intern_type_cache internCache;
+  IDToType indexToType;
+  TypeCache typeCache;
   auto &bodyRegion = (*module).getBodyRegion();
   for (auto &bodyBlock : bodyRegion) {
     for (auto &func : bodyBlock) {
@@ -91,11 +129,12 @@ int main(int argc, char *argv[]) {
       protocir::CIRFunctionID pFunctionID;
       pFunction.mutable_id()->mutable_module_id()->CopyFrom(pModuleID);
       pFunction.mutable_id()->set_id(++func_idx);
-      unsigned long inst_line = 0;
+      IDToOperation indexToOp;
+      OperationCache opCache;
       for (auto &block : cast<cir::FuncOp>(func).getFunctionBody()) {
         for (auto &inst : block) {
           auto pInst = pFunction.add_operations();
-          serializeOperation(inst, pInst, inst_line, pModuleID, internCache);
+          serializeOperation(inst, pInst, pModuleID, typeCache, opCache);
         }
       }
     }
