05610a0060a7029ceec953c32dc6ae2eef208737
Daniil Logunov <43185213+DanielELog@users.noreply.github.com>
2025-03-12 15:14:10 +0300

    feat: improves test run error handling

--- .github/workflows/run_gsac_tests.py
+++ .github/workflows/run_gsac_tests.py
@@ -1,48 +1,32 @@
 #!/usr/bin/python3
 
-import subprocess
 import os
 import sys
 import filecmp
 
-from pathlib import Path
+from utils import *
 
 
 CIR_ORIGINAL = "test.s"
 DESERIALIZED_FILE = "test.cir"
 
 
-def get_test_cmd_line(cir_tac_path, test_path):
-    script_path = os.path.join(cir_tac_path, "run_ser_deser_cycle.py")
-    return "{2} \"{0}\" \"{1}\" > test.out".format(cir_tac_path, test_path, script_path)
-
-
-def filter_ast_attrs(file_path):
-    with open(file_path, "r") as file:
-        code = file.read().replace(" #cir.record.decl.ast", "")
-    with open(file_path, "w") as file:
-        file.write(code)
-
-
-def get_test_paths(root):
-    return [x.absolute().as_posix() for x in Path(root).rglob("*.c")]
-
-
-def remove_if_exists(file_path):
-    if os.path.exists(file_path):
-        os.remove(file_path)
-
-
-def run_test(test_path, cir_tac_path):
+def run_and_check_test_result(test_path, cir_tac_path):
     # preparing files for the test run to have a clean state
     remove_if_exists(CIR_ORIGINAL)
     remove_if_exists(DESERIALIZED_FILE)
 
-    test_cmd = get_test_cmd_line(cir_tac_path, test_path)
-    subprocess.run(test_cmd, shell=True)
+    test_res = run_test(cir_tac_path, test_path)
 
-    # removing sometimes appearing empty ast attributes
-    filter_ast_attrs(CIR_ORIGINAL)
+    if test_res != TestResult.Success:
+        return False
+
+    if not os.path.exists(CIR_ORIGINAL):
+        print("Test succeeded but no original cir file found!")
+        return False
+    if not os.path.exists(DESERIALIZED_FILE):
+        print("Test succeeded but no deserialized file found!")
+        return False
 
     return filecmp.cmp(DESERIALIZED_FILE, CIR_ORIGINAL)
 
@@ -52,13 +36,13 @@ def main():
         print("Expected paths to cir-tac and GSAC directories!")
         return -1
 
-    cir_tac_path = os.path.expanduser(sys.argv[1])
-    gsac_path = os.path.expanduser(sys.argv[2])
+    cir_tac_path = sys.argv[1]
+    gsac_path = sys.argv[2]
     test_files = get_test_paths(gsac_path)
 
     for test in test_files:
         print("Testing file [{0}]\n".format(test))
-        if not run_test(test, cir_tac_path):
+        if not run_and_check_test_result(test, cir_tac_path):
             print("Failure!")
             return 1
         print("Success!\n")
--- run_ser_deser_cycle.py
+++ run_ser_deser_cycle.py
@@ -11,7 +11,14 @@ def create_file_name(name, ext):
 
 
 def run_translation_cmd(cmd, fr, to):
-    subprocess.run("{0} {1} > {2}".format(cmd, fr, to), shell=True)
+    return subprocess.run("{0} {1} > {2}".format(cmd, fr, to), shell=True).returncode == 0
+
+
+def filter_ast_attrs(file_path):
+    with open(file_path, "r") as file:
+        code = file.read().replace(" #cir.record.decl.ast", "")
+    with open(file_path, "w") as file:
+        file.write(code)
 
 
 def main():
@@ -30,9 +37,19 @@ def main():
     test_ser = create_file_name(test_name, "proto")
     test_deser = create_file_name(test_name, "cir")
 
-    subprocess.run("clang -S -Xclang -emit-cir-flat -o {1} {0}".format(test_src, test_cir), shell=True)
-    run_translation_cmd(ser_tool_path, test_cir, test_ser)
-    run_translation_cmd(des_tool_path, test_ser, test_deser)
+    compile_cmd = "clang -S -Xclang -emit-cir-flat -o {1} {0}".format(test_src, test_cir)
+    if subprocess.run(compile_cmd, shell=True).returncode != 0:
+        print("Compile error!")
+        return 1
+    if not run_translation_cmd(ser_tool_path, test_cir, test_ser):
+        print("Serialization error!")
+        return 2
+    if not run_translation_cmd(des_tool_path, test_ser, test_deser):
+        print("Deserialization error!")
+        return 3
+
+    # removing sometimes appearing empty ast attributes
+    filter_ast_attrs(test_cir)
 
     print("\n\nDIFF OUTPUT:\n\n")
     subprocess.run("diff {0} {1}".format(test_cir, test_deser), shell=True)
