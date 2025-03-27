6ee6103a7478557ab34a6c899cf80994e227252d
Daniil Logunov <43185213+DanielELog@users.noreply.github.com>
2025-03-27 13:45:55 +0300

    feat: adds check for tblgen'erated files

--- scripts/build_proto_files.py
+++ scripts/build_proto_files.py
@@ -3,7 +3,8 @@
 import os
 import subprocess
 import sys
-from enum import Enum
+
+import cir_tblgen_util
 
 
 def get_include_args(clangir_path):
@@ -32,124 +33,26 @@ def run_tblgen_command(clangir_path, subcmd, td_file, result_path):
     subprocess.run(cmd, check=True, shell=True)
 
 
-def get_proto_path(file_name):
-    return os.path.join("proto", file_name)
-
-
-def get_decl_path(file_name):
-    return os.path.join("include", "cir-tac", file_name)
-
-
-def get_src_path(file_name):
-    return os.path.join("src", file_name)
-
-
-class CodeType(Enum):
-    Proto = 1
-    Decl = 2
-    Src = 3
-
-
-MAP_TYPE_TO_PATH_GETTER = {
-    CodeType.Proto: get_proto_path,
-    CodeType.Decl: get_decl_path,
-    CodeType.Src: get_src_path,
-}
-
-
-def gen_file(clangir_path, subcmd, td_file, file_name, file_type):
-    file_path = MAP_TYPE_TO_PATH_GETTER[file_type](file_name)
-    subprocess.run("rm -f {0}".format(file_path), shell=True)
-    run_tblgen_command(clangir_path, subcmd, td_file, file_path)
-
-
-def gen_all(clangir_path, subcmd, td_file, name):
-    cpp_name = str.capitalize(name)
-    gen_file(
-        clangir_path,
-        subcmd,
-        td_file,
-        "{0}.proto".format(name),
-        CodeType.Proto,
-    )
-    gen_file(
-        clangir_path,
-        "{0}-serializer-header".format(subcmd),
-        td_file,
-        "{0}Serializer.h".format(cpp_name),
-        CodeType.Decl,
-    )
-    gen_file(
-        clangir_path,
-        "{0}-serializer-source".format(subcmd),
-        td_file,
-        "{0}Serializer.cpp".format(cpp_name),
-        CodeType.Src,
-    )
-    # gen_file(clangir_path, "{0}-deserializer-header", td_file,
-    #          "EnumDeserializer.h", CodeType.Decl)
-    # gen_file(clangir_path, "{0}-deserializer-source", td_file,
-    #          "EnumDeserializer.cpp", CodeType.Src)
+def gen_file(clangir_path, result_dir, file_info: cir_tblgen_util.TblgenFileInfo):
+    result_path = os.path.join(result_dir, file_info.path)
+    subprocess.run("rm -f {0}".format(result_path), shell=True)
+    run_tblgen_command(clangir_path, file_info.subcmd, file_info.td, result_path)
 
 
 def main():
-    if len(sys.argv) != 3:
-        print("Expected paths to clangir and cir-tac directories!")
+    argc = len(sys.argv)
+    if not (3 <= argc <= 4):
+        print("Expected paths to clangir, cir-tac directories"
+              "and optionally resulting dir!")
         return -1
-    clangir = sys.argv[1]
-    cir_tac = sys.argv[2]
+    clangir = os.path.expanduser(sys.argv[1])
+    cir_tac = os.path.expanduser(sys.argv[2])
+    result_dir = cir_tac if argc == 3 else os.path.expanduser(sys.argv[3])
 
-    os.chdir(os.path.expanduser(cir_tac))
+    os.chdir(cir_tac)
 
-    gen_file(
-        clangir,
-        "gen-enum-proto-deserializer-header",
-        "CIROps.td",
-        "EnumDeserializer.h",
-        CodeType.Decl,
-    )
-    gen_file(
-        clangir,
-        "gen-enum-proto-deserializer-source",
-        "CIROps.td",
-        "EnumDeserializer.cpp",
-        CodeType.Src,
-    )
-
-    gen_file(
-        clangir,
-        "gen-op-proto-deserializer-header",
-        "CIROps.td",
-        "OpDeserializer.h",
-        CodeType.Decl,
-    )
-    gen_file(
-        clangir,
-        "gen-op-proto-deserializer-source",
-        "CIROps.td",
-        "OpDeserializer.cpp",
-        CodeType.Src,
-    )
-
-    gen_file(
-        clangir,
-        "gen-attr-proto-deserializer-source",
-        "MLIRCIRAttrs.td",
-        "AttrDeserializer.cpp",
-        CodeType.Src,
-    )
-    gen_file(
-        clangir,
-        "gen-attr-proto-deserializer-header",
-        "MLIRCIRAttrs.td",
-        "AttrDeserializer.h",
-        CodeType.Decl,
-    )
-
-    gen_all(clangir, "gen-op-proto", "CIROps.td", "op")
-    gen_all(clangir, "gen-enum-proto", "CIROps.td", "enum")
-    gen_all(clangir, "gen-type-proto", "MLIRCIRTypes.td", "type")
-    gen_all(clangir, "gen-attr-proto", "MLIRCIRAttrs.td", "attr")
+    for file_info in cir_tblgen_util.get_tblgen_file_infos():
+        gen_file(clangir, result_dir, file_info)
 
 
 if __name__ == "__main__":
