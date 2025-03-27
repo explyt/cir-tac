e6bfc783359fd66b6abaff833811a76da988cbaf
Daniil Logunov <43185213+DanielELog@users.noreply.github.com>
2025-03-27 14:26:30 +0300

    feat: forces CI to upload every failed test

--- .github/workflows/run_gsac_tests.py
+++ .github/workflows/run_gsac_tests.py
@@ -3,18 +3,20 @@
 import filecmp
 import os
 import sys
+import shutil
 
 import utils
 
 CIR_ORIGINAL = "test.s"
 DESERIALIZED_FILE = "test.cir"
+TEST_OUTPUT = "test.out"
 
 
 def run_and_check_test_result(test_path, cir_tac_path, clang):
     # preparing files for the test run to have a clean state
     utils.remove_if_exists(CIR_ORIGINAL)
     utils.remove_if_exists(DESERIALIZED_FILE)
-    utils.remove_if_exists("test.out")
+    utils.remove_if_exists(TEST_OUTPUT)
 
     test_res = utils.run_test(
         cir_tac_path, test_path, enable_output=True, custom_clang=clang
@@ -34,27 +36,37 @@ def run_and_check_test_result(test_path, cir_tac_path, clang):
     return filecmp.cmp(DESERIALIZED_FILE, CIR_ORIGINAL)
 
 
+def save_failed_test(test_path, gsac_path):
+    test_relpath = os.path.relpath(test_path, gsac_path)
+    test_name = test_relpath.replace(os.path.sep, "_")
+    for test_outs in [CIR_ORIGINAL, DESERIALIZED_FILE, TEST_OUTPUT]:
+        save_copy_name = "{0}_{1}".format(test_name, test_outs)
+        save_copy_path = os.path.join("failures", save_copy_name)
+        shutil.copyfile(test_outs, save_copy_path)
+
+
 def main():
     argc = len(sys.argv)
     if argc < 3 or argc > 4:
         print("Expected paths to cir-tac and GSAC directories, optionally to clang!")
         return -1
 
-    cir_tac_path = sys.argv[1]
-    gsac_path = sys.argv[2]
+    cir_tac_path = os.path.expanduser(sys.argv[1])
+    gsac_path = os.path.expanduser(sys.argv[2])
 
     clang = "clang" if argc == 3 else os.path.expanduser(sys.argv[3])
 
     test_files = utils.get_test_paths(gsac_path)
 
+    res = 0
     for test in test_files:
         print("Testing file [{0}]\n".format(test))
         if not run_and_check_test_result(test, cir_tac_path, clang):
-            print("Failure!")
-            return 1
+            print("Failure! Saving test outputs...\n")
+            save_failed_test(test, gsac_path)
+            res = 1
         print("Success!\n")
-
-    return 0
+    return res
 
 
 if __name__ == "__main__":
